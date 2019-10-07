ade57cbe9baf69e026241130d6a75a4e22429334
chris <mail@chris007.de>
2019-10-07 22:10:28 +0200

    Integrated Wifi Updater

--- src/main.cpp
+++ src/main.cpp
@@ -20,14 +20,16 @@ Contact: bt-trx.com, mail@bt-trx.com
 
 #include <Arduino.h>
 
-#include "bttrx_fsm.h"
 #include "git_revision.h"
 #include "settings.h"
 
-// Wifi
-#include "settings_wifi.h"
+#include "bttrx_fsm.h"
+#include "bttrx_wifi.h"
 
+
+WebServer server;
 BTTRX_FSM bttrx_fsm;
+BTTRX_WIFI bttrx_wifi;
 
 void setup()
 {
@@ -68,67 +70,19 @@ void setup()
 	header.append(GIT_REVISION);
 	SERIAL_DBG.println(header.c_str());
 
-	// WIFI
-	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-	  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-	/*use mdns for host name resolution*/
-  if (!MDNS.begin(host)) { //http://bt-trx.local
-    Serial.println("Error setting up MDNS responder!");
-    while (1) {
-      delay(1000);
-    }
-  }
-  Serial.println("mDNS responder started");
-
-	/*return index page which is stored in serverIndex */
-  server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex.c_str());
-  });
-  server.on("/serverIndex", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", serverIndex.c_str());
-  });
-  /*handling uploading firmware file */
-  server.on("/update", HTTP_POST, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
-  }, []() {
-    HTTPUpload& upload = server.upload();
-    if (upload.status == UPLOAD_FILE_START) {
-      Serial.printf("Update: %s\n", upload.filename.c_str());
-      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_WRITE) {
-      /* flashing firmware to ESP*/
-      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-        Update.printError(Serial);
-      }
-    } else if (upload.status == UPLOAD_FILE_END) {
-      if (Update.end(true)) { //true to set the size to the current progress
-        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-      } else {
-        Update.printError(Serial);
-      }
-    }
-  });
-  server.begin();
-
+	ulong startTime = millis();
+	while (!digitalRead(PIN_BTN_0))
+	{
+		ulong currentTime = millis();
+		if (startTime + BTN_PRESS_WIFI_MODE_TIMEOUT < currentTime)
+		{
+			bttrx_wifi.setup();
+			bttrx_wifi.run();
+		}
+	}
 }
 
 void loop()
 {
 	bttrx_fsm.run();
-	server.handleClient();
 }
