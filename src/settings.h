ade6b1032c042c80bdcf7007c2efeb43060367bf
chris <mail@chris007.de>
2019-10-21 21:48:47 +0200

    Move GPIO pin definitions to pins.h

--- src/settings.h
+++ src/settings.h
@@ -20,48 +20,18 @@ Contact: bt-trx.com, mail@bt-trx.com
 
 #pragma once
 
-#ifdef ESP32
-// Bindings for dev-board v4 and newer
-// ESP32 Pin Info:
-// GPIO 6-11: Do not use! (onboard SPI Flash)
-// GPIO 34-39: Input only
-// GPIO 32-39: ADC1
-// ADC2 is not available when using Wifi!
-// in general, pins > 17 are safe to use
-#define SERIAL_BT Serial2 // Default: RX: 16, TX: 17, RTS: 7, CTS: 8
-#define SERIAL_DBG Serial
-
-#define PIN_BTN_0 0      // active low
-#define PIN_LED_BLUE 25  // active high
-#define PIN_LED_GREEN 26 // active high
-#define PIN_PTT_IN 32    // active low
-#define PIN_PTT_OUT 33   // active low
-#define PIN_PTT_LED 12   // active high
-#define PIN_BT_RESET 4   // active low
-#endif
-
-#ifdef TEENSY32
-// Legacy bindings for dev-board v2 and v3
-#define SERIAL_BT Serial3
-#define SERIAL_DBG Serial
-
-#define PIN_BTN_0 23    // active low
-#define PIN_LED_BLUE 3  // active high
-#define PIN_LED_GREEN 4 // active high
-#define PIN_PTT_IN 5    // active Low
-#define PIN_PTT_OUT 6   // active Low
-#define PIN_BT_RESET 13 // active low
-#endif
+#include "pins.h"
 
 #define SERIAL_DBG_RATE 115200
 #define SERIAL_BT_RATE 115200
-#define SERIAL_TIMEOUT 10                // serial readline timeout
-#define DEBUG_CONNECT_TIMEOUT 3000       // Needed for Teensy startup
-#define BTN_PRESS_WIFI_MODE_TIMEOUT 5000 // ms
-
+#define SERIAL_TIMEOUT 10 //ms  // serial readline timeout
 #define SERIAL_DELIMITER '\n'
 #define SERIAL_MAX_LINE_LENGTH 100
 #define BT_SERIAL_TIMEOUT 100 // ms  // Time to wait for answers from BT module
 
 #define INQUIRY_DURATION "5"  // *1.28s
 #define PTT_TURNOFF_DELAY 500 // ms
+#define BTN_PRESS_WIFI_MODE_TIMEOUT 5000 // ms
+
+// Teensy specific
+#define DEBUG_CONNECT_TIMEOUT 3000       // Needed for Teensy startup
